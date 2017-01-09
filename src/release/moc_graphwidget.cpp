32152d8c2bd35a6877f52963825247a306c1729b
lihanyan <2758534967@qq.com>
2017-01-09 22:27:27 +0800

    Add files via upload

---
+++ src/release/moc_graphwidget.cpp
@@ -0,0 +1,88 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'graphwidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../graphwidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'graphwidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_GraphWidget_t {
+    QByteArrayData data[1];
+    char stringdata[13];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_GraphWidget_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_GraphWidget_t qt_meta_stringdata_GraphWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 11)
+    },
+    "GraphWidget\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_GraphWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void GraphWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObject GraphWidget::staticMetaObject = {
+    { &QGraphicsView::staticMetaObject, qt_meta_stringdata_GraphWidget.data,
+      qt_meta_data_GraphWidget,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *GraphWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *GraphWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_GraphWidget.stringdata))
+        return static_cast<void*>(const_cast< GraphWidget*>(this));
+    return QGraphicsView::qt_metacast(_clname);
+}
+
+int GraphWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGraphicsView::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    return _id;
+}
+QT_END_MOC_NAMESPACE
